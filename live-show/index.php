0eef3ba1e41aaedca65d5c61a9f0d8fbec9da01e
Caleb <caleb@eddukate.ga>
2020-01-02 18:35:46 -0500

    Live-show
    Corrigiendo errores y modificando la pagina para que sea modo Dark en los  Lives y en los cursos.

---
+++ live-show/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eddukate Live - Lo mejor en tecnologia.</title>
+     <!-- Favicons -->
+     <link href="../public/favicon.png" rel="icon">
+     <!-- Estilos -->
+     <link rel="stylesheet" href="../public/css/style.css">
+     <link rel="stylesheet" href="../public/css/styles.css">
+     <!-- Bootstrap CSS File -->
+     <link href="../public/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+ 
+     <!-- Libraries CSS Files -->
+     <link href="../public/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+     <link href="../public/lib/animate/animate.min.css" rel="stylesheet">
+     <link href="../public/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+     <link href="../public/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+     <link href="../public/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+ 
+     <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js"></script>
+     <link href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css" rel="stylesheet">
+</head>
+<body style="background: #222831;">
+     <!-- Header Nav -->
+     <header id="header" class="fixed-top" style="background: #1a252d;">
+        <div class="container">
+
+          <div class="logo float-left">
+            <a href="index.php" class="scrollto"><img src="https://cdneddukate.000webhostapp.com/logo_page/eddukate-logo-web-blanco.png" alt="" class="img-fluid"></a>
+          </div>
+
+          <nav class="main-nav float-right d-none d-lg-block">
+            <ul>
+              <li><a href="cursos/" style="color: white;">Cursos</a></li>
+              <li><a href="login/login.php" class="btn btn-success btn-sm" style="color: white;">Ingresar</a></li>
+              <!--
+              <li><a href="servicios/">Servicios</a></li>
+              <li><a href="Contacto.html">Contacto</a></li>
+              <li><a href="login/" class="btn btn-success" style="color: white;">Ingresar</a></li>
+              -->
+              <br>
+            </ul>
+          </nav><!-- .main-nav -->
+        </div>
+      </header>
+   <!-- / Header Nav --> 
+
+      <br><br><br><br>
+      <section id="video-livestream">
+            <div class="chat-stream" style="background: #22303a;">
+                <div class="chat-title">
+                    <h1>Chat en vivo</h1>
+                    <hr>
+                </div>
+                <div class="write-chat">
+                </div>
+                </div>
+            
+        <div class="video-stream">
+            <!-- Video en Youtube -->
+             <iframe width="820" height="480" src="https://www.youtube.com/embed/cIrDqBSUrYw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <!-- Source Video
+            <video controls height="480" width="820">
+                <source src="video/test.mp4" type="video/mp4">
+            </video>
+            -->
+       </div>
+       <br>
+        <div class="description-live">
+            <h1 style="color: white"> <!-- <span>En vivo</span>--> Eddukate Taller: Mi primera pagina web</h1>
+        </div>
+      </section>
+      
+</body>
+</html>
