15b060ed6a774f83b448640abd9717e8ab908c91
Caleb <caleb@eddukate.ga>
2020-01-02 15:25:18 -0500

    Agregando titulos
    He Agregado
    
     $title_login = Para el login
     $title_register = Para el registro

--- app/helpers/header/header.php
+++ app/helpers/header/header.php
@@ -1,3 +1,8 @@
 <?php 
+
+    /* Titulos para las paginas */
+
     $title = "Descubre tu potencial en Eddukate";
+    $title_login = "Ingresar | Eddukate ";
+    $title_register = "Registro | Eddukate";
 ?>
